ee82cae53debc85bebc6cd8f7d7c3e931e1d2416
Dheeraj Kodimela <120166872+Dheeraj2125@users.noreply.github.com>
2026-02-16 16:56:58 -0500

    Implement user authentication controller with routes Closes #36

---
+++ server/pkg/controller/user_controller.go
@@ -0,0 +1,76 @@
+package controller
+
+import (
+	"github.com/labstack/echo/v4"
+	"ims-intro/pkg/controller/request"
+	"ims-intro/pkg/controller/response"
+	"ims-intro/pkg/service"
+	"net/http"
+	"time"
+)
+
+type UserController struct {
+	userService service.IUserService
+}
+
+func NewUserController(userService service.IUserService) *UserController {
+	return &UserController{userService}
+}
+
+func (controller *UserController) RegisterUserRoutes(e *echo.Echo) {
+	e.POST("/login", controller.Login)
+	e.POST("/signup", controller.SignUp)
+	e.POST("/logout", controller.Logout)
+}
+
+func (controller *UserController) Login(c echo.Context) error {
+	var loginRequest request.LoginRequest
+	err := c.Bind(&loginRequest)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, response.NewErrorResponse("Invalid request: unable to bind the provided data to the user structure"))
+	}
+
+	token, err := controller.userService.Login(loginRequest.Username, loginRequest.Password)
+	if err != nil {
+		return c.JSON(http.StatusUnprocessableEntity, response.NewErrorResponse(err.Error()))
+	}
+
+	cookie := new(http.Cookie)
+	cookie.Name = "token"
+	cookie.Value = token
+	cookie.Expires = time.Now().Add(24 * time.Hour)
+	cookie.Path = "/"
+	cookie.HttpOnly = false
+	cookie.SameSite = http.SameSiteLaxMode
+	c.SetCookie(cookie)
+
+	return c.NoContent(http.StatusOK)
+}
+
+func (controller *UserController) SignUp(c echo.Context) error {
+	var signUpRequest request.SignUpRequest
+	err := c.Bind(&signUpRequest)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, response.NewErrorResponse("Invalid request: unable to bind the provided data to the user structure"))
+	}
+
+	err = controller.userService.SignUp(signUpRequest.ToDtoModel())
+	if err != nil {
+		return c.JSON(http.StatusUnprocessableEntity, response.NewErrorResponse(err.Error()))
+	}
+
+	return c.NoContent(http.StatusCreated)
+}
+
+func (controller *UserController) Logout(c echo.Context) error {
+	cookie := new(http.Cookie)
+	cookie.Name = "token"
+	cookie.Value = ""
+	cookie.Expires = time.Now().Add(-24 * time.Hour)
+	cookie.Path = "/"
+	cookie.HttpOnly = false
+	cookie.SameSite = http.SameSiteLaxMode
+	c.SetCookie(cookie)
+
+	return c.NoContent(http.StatusOK)
+}
