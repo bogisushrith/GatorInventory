7cc5748e0376c9763e58f73af13fdec69cb25d35
Dheeraj Kodimela <120166872+Dheeraj2125@users.noreply.github.com>
2026-03-25 18:25:41 -0400

    Add pagination support for product responses Closes #77

--- server/pkg/controller/response/productResponse.go
+++ server/pkg/controller/response/productResponse.go
@@ -3,14 +3,28 @@ package response
 import "ims-intro/pkg/domain"
 
 type ProductResponse struct {
+	Id       int64   `json:"id"`
 	Name     string  `json:"name"`
 	Price    float32 `json:"price"`
 	Quantity int64   `json:"quantity"`
 	Category string  `json:"category"`
 }
 
+type ProductPaginationResponse struct {
+	Page       int   `json:"page"`
+	Limit      int   `json:"limit"`
+	Total      int64 `json:"total"`
+	TotalPages int   `json:"total_pages"`
+}
+
+type ProductListResponse struct {
+	Data       []*ProductResponse        `json:"data"`
+	Pagination ProductPaginationResponse `json:"pagination"`
+}
+
 func toProductResponse(product *domain.Product) *ProductResponse {
 	return &ProductResponse{
+		Id:       product.Id,
 		Name:     product.Name,
 		Price:    product.Price,
 		Quantity: product.Quantity,
@@ -24,4 +38,16 @@ func ToProductResponseList(products []*domain.Product) []*ProductResponse {
 		responses = append(responses, toProductResponse(product))
 	}
 	return responses
+}
+
+func ToPaginatedProductResponse(products []*domain.Product, page, limit int, total int64, totalPages int) *ProductListResponse {
+	return &ProductListResponse{
+		Data: ToProductResponseList(products),
+		Pagination: ProductPaginationResponse{
+			Page:       page,
+			Limit:      limit,
+			Total:      total,
+			TotalPages: totalPages,
+		},
+	}
 }
