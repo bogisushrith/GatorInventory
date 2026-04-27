f4e165c5f07f7b86ffb1e775ed7fa3a0f324b9bc
Sushrith Bogi <116748982+bogisushrith@users.noreply.github.com>
2026-04-27 17:01:53 -0400

    Add Email field to SignUpRequest and new UpdateUserRoleRequest Closes #336

--- server/pkg/controller/request/user_request.go
+++ server/pkg/controller/request/user_request.go
@@ -9,13 +9,19 @@ type LoginRequest struct {
 
 type SignUpRequest struct {
 	Username string `json:"username"`
+	Email    string `json:"email"`
 	Password string `json:"password"`
 	Role     string `json:"role"`
+}
+
+type UpdateUserRoleRequest struct {
+	Role string `json:"role"`
 }
 
 func (request *SignUpRequest) ToDtoModel() dto.UserCreate {
 	return dto.UserCreate{
 		Username: request.Username,
+		Email:    request.Email,
 		Password: request.Password,
 		Role:     request.Role,
 	}
